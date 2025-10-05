df9df41be859470a10789d4dc5ee3d9736fbe6d5
Deividson Gonini <deividsongonini@gmail.com>
2025-10-05 15:59:13 -0300

    ajuste nome caminho

--- authentication-function/AuthenticationFunction/src/main/java/authentication/App.java
+++ authentication-function/AuthenticationFunction/src/main/java/authentication/App.java
@@ -1,4 +1,4 @@
-package helloworld;
+package authentication;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
--- authentication-function/AuthenticationFunction/src/test/java/authentication/AppTest.java
+++ authentication-function/AuthenticationFunction/src/test/java/authentication/AppTest.java
@@ -1,10 +1,8 @@
-package helloworld;
+package authentication;
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import org.junit.Test;
 
 public class AppTest {
 //  @Test
